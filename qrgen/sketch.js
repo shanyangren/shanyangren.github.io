4a4793bb12c65a2606cc9c29529ebc259469c480
shanyangren <shanyangren@example.com>
2026-05-12 14:45:50 +0900

    按钮+居中的更改

--- qrgen/sketch.js
+++ qrgen/sketch.js
@@ -1,23 +1,38 @@
 let qrDate;
-
+let urlInput;
+let colorInput;
+let backgroundInput;
+let button;
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  //输入二维码栏
+  urlInput = select('#url');
 
-  //把网址转换成二维码
-  qrDate = qr.encodeQR('https://google.com');
+  colorInput = select('#color');
 
-  console.log(qrDate);
+  backgroundInput = select('#background');
+
+  button =select('#save');
+  button.mousePressed(saveQR);//按按钮呼唤saveQR 
+
+  // console.log(qrDate);
 }
 
 function draw() {
-  background(21, 200, 120);
+  background(backgroundInput.value());
 
-  let gap = 10;
+  let gap = 13;
 //颜色
-  fill(50,20,40);
+  fill(colorInput.value());
+    //把网址转换成二维码
+  qrDate = qr.encodeQR(urlInput.value());
 
+  let w = gap * qrDate.length;
+  let h =w;
+  
+  translate(250-w/2+gap/2,250-h/2+gap/2);
   for (let y = 0; y < qrDate.length; y++) {
     let row = qrDate[y];//列队
     for (let x = 0; x < row.length; x++) {
@@ -25,11 +40,13 @@ function draw() {
       //格子是true的话
       if (cell) {
         circle(x * gap, y * gap, gap);
-
-
       }
     }
   }
 
+ 
 }
-
+//作为图片保存qr
+function saveQR(){
+  saveCanvas('qr.png');
+}
